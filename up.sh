d3157506fd46da33b466ff8bd6c5f2fcd3216523
Namir Taajwar Mahmud <namirtaajwarmahmud@gmail.com>
2026-07-04 17:29:11 +0600

    Update author and website information in up.sh

--- up.sh
+++ up.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 #set -e
 ##################################################################################################################
-# Author    : Erik Dubois
-# Website   : https://www.erikdubois.be
-# Website   : https://www.alci.online
-# Website   : https://www.ariser.eu
-# Website   : https://www.arcolinux.info
-# Website   : https://www.arcolinux.com
-# Website   : https://www.arcolinuxd.com
-# Website   : https://www.arcolinuxb.com
-# Website   : https://www.arcolinuxiso.com
-# Website   : https://www.arcolinuxforum.com
+# Author 	: Namir Taajwar mahmud
+# Website   : https://www.namir.page.gd
+# Website   : https://www.opensabayon.infinityfreeapp.com
+# Website	: https://www.opensabayon.discourse.group
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
